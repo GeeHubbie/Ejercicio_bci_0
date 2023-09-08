bab06ea621252eee3bc86a03b00b31b91a925880
Gerardo Campos <gerardo.campos@globallogic.com>
2023-09-08 17:57:37 -0300

    Se agregaron clases de testing para clases con rutinas de validación y encriptación

---
+++ app/src/test/java/utilities/PasswordEncryptorSpec.groovy
@@ -0,0 +1,20 @@
+package utilities
+
+import spock.lang.Specification
+
+class PasswordEncryptorSpec extends Specification{
+
+    def 'Dado un valor, verificar que al encriptarlo y desencriptarlo, el obtenido es igual al original'() {
+
+        given: 'un valor que se desea encriptar'
+            String original = "OriginaL"
+            def krypto = new PasswordEncryptor()
+            String encriptado = krypto.convertToDatabaseColumn(original)
+        when: 'se desencripta el valor encriptado ...'
+            String devuelta = krypto.convertToEntityAttribute(encriptado)
+        then: 'el valor obtenido es igual al original'
+            original == devuelta
+    }
+
+
+}
---
+++ app/src/test/java/utilities/ValidadorSpec.groovy
@@ -0,0 +1,48 @@
+package utilities
+
+import spock.lang.Specification
+import spock.lang.Unroll
+import sun.security.util.Password
+
+class ValidadorSpec extends Specification{
+
+    @Unroll
+    def 'El resultado de ejecutar emailValidator sobre el email: #inputEmail es: #resultadoEsperado'() {
+
+        given: 'Dada una dirección de correo, evaluar si cumple las reglas de validación definidas'
+            Validador juez = new Validador()
+            Boolean resultado
+        when: 'se evalúa la dirección de correo recibida como entrada'
+             resultado = juez.emailValidator(inputEmail)
+        then:
+            resultado == resultadoEsperado
+        where:
+        inputEmail          | resultadoEsperado
+        gee@bci.com         |   true
+        gee@bci             |   false
+        un4@bci.com         |   false
+        3.1416              |   false
+        GeeCampos@bci.co    |   true
+    }
+
+    @Unroll
+    def 'Resultados del validador de password: #inputpwd --> #resultadoEsperado'()
+    {
+        given: 'determinar si un password cumple los criterios establecidos'
+            Validador juez = new Validador()
+            Boolean resultado
+        when:
+            resultado = juez.passwordValidator(inputpwd)
+        then:
+            resultado == resultadoEsperado
+        where:
+            inputpwd            |   resultadoEsperado
+            Pas5w0rd            |   true
+            password            |   false
+            Passw0rd            |   false
+            minusculas          |   false
+            Password            |   false
+            Muchos999999        |   true
+            Demasiados2023      |   false
+    }
+}
