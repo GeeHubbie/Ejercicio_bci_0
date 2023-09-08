541e27532d12cc20aca99c38878b0b239207216b
Gerardo Campos <gerardo.campos@globallogic.com>
2023-09-08 15:53:48 -0300

    Se resolvieron errores de sintaxis de Groovy en los tests

--- app/src/test/java/controller/ConsultarUsuarioSpec.groovy
+++ app/src/test/java/controller/ConsultarUsuarioSpec.groovy
@@ -1,88 +1,108 @@
 package controller
 
-import exceptions.ErrorMessage
 import models.entity.PhoneNumber
 import models.entity.Usuario
+import org.springframework.http.HttpStatus
+import service.UsuarioServiceImpl
 import spock.lang.Specification
 import utilities.TokenUtility
-import service.IUsuarioService
-import spock.mock.MockingApi
 
 class ConsultarUsuarioSpec extends Specification{
 
-    def  setup() {
-
-        def userId = "45bcf2ac-f68b-4a85-9d47-f87ec72ad10a"
-        def token = 'dummyToken'
-
-        def nuevo = new Usuario()
-        nuevo.userId = UUID.fromString(userId)
-        nuevo.email = 'user@dominio.com'
-        nuevo.password = 'Password01'
-        nuevo.name = 'Pepe Cortisona'
-        nuevo.isActive = true
-        nuevo.lastLogin = null
-        def tlf = new PhoneNumber()
-        tlf.countryCode = '+56'
-        tlf.cityCode = 9
-        tlf.number = 71328555
-        nuevo.phones.add(tlf)
-        tlf.number = 75251219
-        nuevo.phones.add(tlf)
-        TokenUtility utility = Stub()
-        IUsuarioService servicio = Stub()
-        Optional<Usuario> optionalUser
-    }
-
     def 'Consultar un usuario por ID - Caso exitoso'() {
 
-        given: 'un id de un usuario que existe y un Token válido (JWT)'
+        given: 'un id (45bcf2ac-f68b-4a85-9d47-f87ec72ad10a) de un usuario que existe y un Token válido (JWT)'
 
-        servicio.getUsuarioById(userId) >> Optional(nuevo)
+            def userId = "45bcf2ac-f68b-4a85-9d47-f87ec72ad10a"
+            def uuid = UUID.fromString(userId)
+            def nuevo = new Usuario()
+            nuevo.userId = UUID.fromString(userId)
+            nuevo.email = 'user@dominio.com'
+            nuevo.password = 'Password01'
+            nuevo.name = 'Pepe Cortisona'
+            nuevo.isActive = true
+            nuevo.lastLogin = null
+            def tlf = new PhoneNumber()
+            tlf.countryCode = '+56'
+            tlf.cityCode = 9
+            tlf.number = 71328555
+            nuevo.phones.add(tlf)
+            tlf.number = 75251219
+            nuevo.phones.add(tlf)
+
+            def tokenUtil
+            tokenUtil = Stub(TokenUtility.class)
+
+            def  servicio
+            servicio = Stub(UsuarioServiceImpl.class)
+
+            def token
+            token = "dummyToken"
+            tokenUtil.isTokenValid(token) >> true
+
+            Optional<Usuario> maybe
+            maybe = Optional.of(nuevo)
+            servicio.getUsuarioById(uuid) >> maybe
 
         when: 'consultamos la información del usuario con ID="45bcf2ac-f68b-4a85-9d47-f87ec72ad10a" '
 
-        utility.isTokenValid(token) == true
-        Optional(nuevo).isPresent == true
+            tokenUtil.isTokenValid(token) == true
+            maybe.isPresent()
 
 
         then: 'se devuelve la información del usuario con ese id'
 
-        Usuario aRetornar = nuevo
-        String token = utility.getToken(nuevo)
-        HttpStatus == 200 OK
+            Usuario aRetornar = nuevo
+            String newToken = tokenUtil.getToken(nuevo)
+            HttpStatus.OK
     }
 
     void 'Consultar un usuario por ID - Caso token inválido'() {
 
         given: 'un id de usuario y un Token inválido (JWT)'
 
-        def Boolean isTokenValid = utility.isTokenValid(token)
+            def tokenEnviado = 'wrong token'
+            def tokenUtil
+            tokenUtil = Stub(TokenUtility.class)
+            tokenUtil.isTokenValid(tokenEnviado) >> false
+            Boolean isTokenValid = tokenUtil.isTokenValid(tokenEnviado)
 
         when: 'consultamos la información del usuario enviando un token inválido (endpoint: login/{userId})'
 
-        isTokenValid == false
+            isTokenValid == false
 
         then: 'se devuelve BAD REQUEST con ERROR código 111, msj = INVALID TOKEN'
 
-        def e = thrown(InvalidTokenException)
+            Exception e = thrown(InvalidTokenException)
+            HttpStatus.BAD_REQUEST
 
     }
 
-    void 'Consultar un usuario por ID - Caso usuario no existe'() {
+    void 'Consultar un usuario por ID - Caso token válido pero usuario no existe'() {
 
         given: 'No existe un usuario con id suministrado '
 
-        servicio.getUsuarioById(userId) >> optionalUser
-        utility.isTokenValid(token) == true
+            def  servicio
+            servicio = Stub(UsuarioServiceImpl.class)
+            def tokenUtil
+            tokenUtil = Stub(TokenUtility.class)
+            def tokenEnviado = 'dummy token'
+            tokenUtil.isTokenValid(tokenEnviado) >> true
+            def Optional<Usuario> optionalUser = Optional.empty()
+            def idEnviado = "45bcf2ac-f68b-4a85-9d47-f87ec72ad10a"
+            def uuid = UUID.fromString(idEnviado)
+            servicio.getUsuarioById(uuid) >> optionalUser
+
+            Boolean isTokenValid = tokenUtil.isTokenValid(tokenEnviado)
 
         when: 'No existe un usuario con id suministrado (endpoint: login/{userId})'
 
-        optionalUser.isPresent() == false
+            isTokenValid == true
+            optionalUser.isPresent() == false
 
         then: 'se devuelve HttpStaus NOT FOUND'
 
-        HttpStatus == HttpStatus.NOT_FOUND
+            HttpStatus.NOT_FOUND
 
     }
 
--- app/src/test/java/controller/CrearUsuarioSpec.groovy
+++ app/src/test/java/controller/CrearUsuarioSpec.groovy
@@ -4,119 +4,119 @@ import exceptions.ErrorMessage
 import models.entity.PhoneNumber
 import models.entity.Usuario
 import request.UsuarioRequest
+import spock.lang.Specification
 import utilities.TokenUtility
 import utilities.Validador
 
-class CrearUsuarioSpec {
-
-    def  setup() {
-
-        Validador validador = Stub()
-        TokenUtility utility = Stub()
-    }
+class CrearUsuarioSpec extends Specification{
 
     void 'Crear un usuario - Caso exitoso - (endpoint: /sign-up)'() {
 
         given: 'Crear usuario con información enviada, una vez validada'
 
-        UsuarioRequest  ureq = new UsuarioRequest()
-        ureq.email = 'user@bci.com'
-        ureq.password = 'Password01'
-        ureq.nombre = 'Pepe Cortisona'
-        PhoneNumber tlf = new PhoneNumber()
-        tlf.countryCode = '+56'
-        tlf.cityCode = 9
-        tlf.number = 71328555
-        ureq.userPhones.add(tlf)
-        tlf.number = 75251219
-        ureq.userPhones.add(tlf)
+            Validador validador = new Validador()
+            TokenUtility tokenUtil = new TokenUtility()
+            UsuarioRequest  ureq = new UsuarioRequest()
+            ureq.email = 'user@bci.com'
+            ureq.password = 'Password01'
+            ureq.nombre = 'Pepe Cortisona'
+            PhoneNumber tlf = new PhoneNumber()
+            tlf.countryCode = '+56'
+            tlf.cityCode = 9
+            tlf.number = 71328555
+            ureq.userPhones.add(tlf)
+            tlf.number = 75251219
+            ureq.userPhones.add(tlf)
 
-        validador.emailValidator(u.email) >> true
-        validador.passwordValidator(u.password) >> true
+            Boolean emailValido = validador.emailValidator(ureq.email)
+
+            Boolean pwdValido = validador.passwordValidator(ureq.password)
 
 
         when: 'se recibe un request para crear un usuario y los datos recibidos pasan reglas de validación'
 
-        validador.emailValidator(u.email) == true
-        validador.passwordValidador(u.password) == true
+            emailValido == true
+            pwdValido == true
 
         then: 'se crea usuario y se responde con registro de usuario y token válido'
 
-        def nuevo = new Usuario()
-        nuevo.userId = UUID.fromString(userId)
-        nuevo.email = 'user@bci.com'
-        nuevo.password = 'Password01'
-        nuevo.name = 'Pepe Cortisona'
-        nuevo.isActive = true
-        nuevo.lastLogin = null
-        def fono = new PhoneNumber()
-        fono.countryCode = '+56'
-        fono.cityCode = 9
-        fono.number = 71328555
-        nuevo.phones.add(fono)
-        fono.number = 75251219
-        nuevo.phones.add(fono)
+            def nuevo = new Usuario()
+            nuevo.userId = UUID.randomUUID()
+            nuevo.email = 'user@bci.com'
+            nuevo.password = 'Password01'
+            nuevo.name = 'Pepe Cortisona'
+            nuevo.isActive = true
+            nuevo.lastLogin = null
+            def fono = new PhoneNumber()
+            fono.countryCode = '+56'
+            fono.cityCode = 9
+            fono.number = 71328555
+            nuevo.phones.add(fono)
+            fono.number = 75251219
+            nuevo.phones.add(fono)
 
-        String newToken ="newToken"
-        utility.getToken(nuevo) >> newToken
-        HttpStatus == 201 CREATED
+            String newToken = tokenUtil.getToken(nuevo)
+            HttpStatus == 201 CREATED
     }
 
     void 'Crear usuario - Caso email inválido - (endpoint: /sign-up)'() {
 
         given: 'Procesar request de creación de un usuario donde email no cumple criterios de validación'
 
-        def  ureq = new UsuarioRequest()
-        ureq.email = 'user@bci'
-        ureq.password = 'Password01'
-        ureq.nombre = 'Pepe Cortisona'
-        def tlf = new PhoneNumber()
-        tlf.countryCode = '+56'
-        tlf.cityCode = 9
-        tlf.number = 71328555
-        ureq.userPhones.add(tlf)
-        tlf.number = 75251219
-        ureq.userPhones.add(tlf)
+            def  ureq = new UsuarioRequest()
+            ureq.email = 'user@bci'
+            ureq.password = 'Password01'
+            ureq.nombre = 'Pepe Cortisona'
+            def tlf = new PhoneNumber()
+            tlf.countryCode = '+56'
+            tlf.cityCode = 9
+            tlf.number = 71328555
+            ureq.userPhones.add(tlf)
+            tlf.number = 75251219
+            ureq.userPhones.add(tlf)
 
-        // email enviado no cumple reglas de validación
-        validador.emailValidator(u.email) >> false
+            // email enviado no cumple reglas de validación
+            Validador validador = new Validador()
+
+            Boolean emailValido = validador.emailValidator(ureq.email)
 
         when: 'email enviado no cumple reglas de validación'
 
-        validador.emailValidator(u.email) == false
+            emailValido == false
 
         then: 'se rechaza solicitud devolviendo mensaje de error BAD REQUEST'
 
-        def ErrorMessage error = new ErrorMessage(333, "INVALID EMAIL ADDRESS")
-        HttpStatus == 400 BAD_REQUEST
+            ErrorMessage error = new ErrorMessage(333, "INVALID EMAIL ADDRESS")
+            HttpStatus == 400 BAD_REQUEST
     }
 
     void 'Crear usuario - Caso password inválido - (endpoint: /sign-up)'() {
 
         given: 'Procesar request de creación de un usuario donde el password no cumple criterios de validación'
 
-        def  ureq = new UsuarioRequest()
-        ureq.email = 'user@bci.com'
-        ureq.password = 'Password'
-        ureq.nombre = 'Pepe Cortisona'
-        def tlf = new PhoneNumber()
-        tlf.countryCode = '+56'
-        tlf.cityCode = 9
-        tlf.number = 71328555
-        ureq.userPhones.add(tlf)
-        tlf.number = 75251219
-        ureq.userPhones.add(tlf)
+            def  ureq = new UsuarioRequest()
+            ureq.email = 'user@bci.com'
+            ureq.password = 'Password'
+            ureq.nombre = 'Pepe Cortisona'
+            def tlf = new PhoneNumber()
+            tlf.countryCode = '+56'
+            tlf.cityCode = 9
+            tlf.number = 71328555
+            ureq.userPhones.add(tlf)
+            tlf.number = 75251219
+            ureq.userPhones.add(tlf)
 
-        validador.passwordValidador(u.password) >> false
+            Validador validador = new Validador()
+            Boolean pwdValido = validador.passwordValidator(ureq.password)
 
         when: 'password enviado no cumple reglas de validación'
 
-        validador.passwordValidador(u.password) == false
+            pwdValido == false
 
         then: 'se rechaza solicitud devolviendo mensaje de error BAD REQUEST'
 
-        def ErrorMessage error = new ErrorMessage(444, "INVALID PASSWORD, PASSWORD DOES NOT MATCH CRITERIA")
-        HttpStatus == 400 BAD_REQUEST
+            def ErrorMessage error = new ErrorMessage(444, "INVALID PASSWORD, PASSWORD DOES NOT MATCH CRITERIA")
+            HttpStatus == 400 BAD_REQUEST
     }
 
 
